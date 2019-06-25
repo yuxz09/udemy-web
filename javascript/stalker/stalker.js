2b3123e781d158e14c7763d054fa54a55b5164b8
yuz <42396093+yuxz09@users.noreply.github.com>
2019-06-25 11:07:18 +0800

    Add files via upload

---
+++ javascript/stalker/stalker.js
@@ -0,0 +1,6 @@
+var firstname = prompt("what is your first name?");
+var lastname = prompt("what is your last name?");
+var age = prompt("what is your age?");
+console.log("name: " + firstname + " " + lastname);
+console.log("age: " + age);
+
